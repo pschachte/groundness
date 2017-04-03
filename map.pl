f333f5d2587b8e4f6f16df86d9a021b8214fc4c8
Peter Schachte <schachte@unimelb.edu.au>
2017-04-03 21:27:52 +1000

    Add copyright and license to source files

--- map.pl
+++ map.pl
@@ -3,7 +3,20 @@
 %  Author   : Peter Schachte
 %  Origin   : Mon Aug 11 17:09:41 2003
 %  Purpose  : Implement finite maps using balanced trees
-%  Copyright: (c) 2004 Peter Schachte.  All rights reserved.
+%  Copyright 2004 Peter Schachte
+%
+%  Licensed under the Apache License, Version 2.0 (the "License");
+%  you may not use this file except in compliance with the License.
+%  You may obtain a copy of the License at
+%
+%      http://www.apache.org/licenses/LICENSE-2.0
+%
+%  Unless required by applicable law or agreed to in writing, software
+%  distributed under the License is distributed on an "AS IS" BASIS,
+%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%  See the License for the specific language governing permissions and
+%  limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 :- module(map, [ map_empty/1,		% the empty map
